18096b08e1de035f4f1196012bea16e6354f506d
George Thayamkery <george.thayamkery@gmail.com>
2019-11-03 01:47:10 -0500

    tie

--- backend/auth/authorizer.js
+++ backend/auth/authorizer.js
@@ -1,10 +1,23 @@
-const isAuthorized = (req, res, next) => {
+exports.isAuthorized = (req, res, next) => {
     let auth = req.cookies.auth;
     if (auth == null || auth == "")
         return res.status(401).send({success: false, error: "Not Authorized"})
     let authObj = JSON.parse(auth);
     if (authObj.loggedIn == false)
         return res.status(401).send({success: false, error: "Not Authorized"});
+    
+    req.user = authObj;
+    return next(req, res);
+}
+
+
+exports.isAuthorizedRedirect = (req, res, next) => {
+    let auth = req.cookies.auth;
+    if (auth == null || auth == "")
+        return res.redirect("/login");
+    let authObj = JSON.parse(auth);
+    if (authObj.loggedIn == false)
+        return res.redirect("/login");
     
     req.user = authObj;
     return next(req, res);
--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const cookieParser = require('cookie-parser')
+const authorizer = require("./backend/auth/authorizer");
 const app = express();
 const port = 3000;
 
+app.use(cookieParser())
+
 app.use('/backend', require('./backend/routes.js'))
-app.get('/', (req, res) => res.send('Hello World!'))
+
+app.use("/static", express.static('../frontend/my-app/build/public'));
+app.get("/", authorizer.isAuthorizedRedirect, (req, res) => {
+    return res.sendFile('frontend/my-app/build/index.html', { root: "./"});
+});
+app.get("/messages", authorizer.isAuthorizedRedirect, (req, res) => {
+    return res.sendFile('frontend/my-app/build/index.html', { root: "./"});
+});
+app.get("/update", authorizer.isAuthorizedRedirect, (req, res) => {
+    return res.sendFile('frontend/my-app/build/index.html', { root: "./"});
+});
+app.get("/inventory", authorizer.isAuthorizedRedirect, (req, res) => {
+    return res.sendFile('frontend/my-app/build/index.html', { root: "./"});
+});
+app.get("/login", (req, res) => {
+    return res.sendFile('frontend/my-app/build/index.html', { root: "./"});
+});
+app.use("/", express.static('frontend/my-app/build/'));
 
 app.listen(port, () => console.log(`Fullhouse listening on port ${port}!`))
